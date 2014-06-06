29f04073bbd521a0ed4e3d4635ecd17b2cfa9d36
tingyu <ytcleartime@gmail.com>
2014-06-06 12:24:58 -0700

    Add Binary

---
+++ Add Binary/AddBinary.java
@@ -0,0 +1,65 @@
+/**
+Add Binary
+
+ Given two binary strings, return their sum (also a binary string).
+
+For example,
+a = "11"
+b = "1"
+Return "100". 
+
+*/
+
+//my solution
+//first padding a or b to the longer length, then consider flag and 0, 1 situation
+//you can use flag to indicate a carrier
+public class Solution {
+    public String addBinary(String a, String b) {
+        if(a.length() < b.length()){
+            String tmp = a;
+            for(int i = 0; i < b.length() - a.length(); i++){
+                tmp = '0' + tmp;
+            }
+            a = tmp;
+        }else if(a.length() > b.length()){
+            String tmp = b ;
+            for(int i = 0; i < a.length() - b.length(); i++){
+                tmp = '0' + tmp;
+            }
+            b = tmp;
+        }
+        
+        String res = "";
+        boolean flag = false;
+        for(int i = a.length()-1; i>=0; i--){
+            if(a.charAt(i) != b.charAt(i)){ // 0+1, 1+0
+                if(flag == false){
+                    res = '1' + res;
+                }else{
+                    flag = true;
+                    res= '0' + res;
+                }
+            }else if(a.charAt(i) == '1'){ //1+1
+                if(flag == false){
+                    flag = true;
+                    res = '0' + res;
+                }else{
+                    flag = true;
+                    res = '1' + res;
+                }
+            }else{ //0 + 0
+                if(flag == false){
+                    res = '0' + res;
+                }else{
+                    flag =false;
+                    res = '1' + res;
+                }
+            }
+        }
+        
+        if(flag == true){
+            res = '1' + res;
+        }
+        return res;
+    }
+}
