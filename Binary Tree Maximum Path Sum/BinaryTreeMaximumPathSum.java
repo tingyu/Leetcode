1d9cb55ceb3cebe6dc24bbe7d118628d994ae574
tingyu <ytcleartime@gmail.com>
2014-06-18 19:11:04 -0700

    Binary Tree Maximum Path Sum

---
+++ Binary Tree Maximum Path Sum/BinaryTreeMaximumPathSum.java
@@ -0,0 +1,83 @@
+/**
+Binary Tree Maximum Path Sum
+
+Given a binary tree, find the maximum path sum.
+
+The path may start and end at any node in the tree.
+
+For example:
+Given the below binary tree,
+
+       1
+      / \
+     2   3
+Return 6.
+*/
+
+/*
+Thoughts
+
+1) Recursively solve this problem
+2) Get largest left sum and right sum
+2) Compare to the stored maximum
+*/
+//http://www.programcreek.com/2013/02/leetcode-binary-tree-maximum-path-sum-java/
+
+// Definition for binary tree
+class TreeNode {
+	int val;
+	TreeNode left;
+	TreeNode right;
+ 
+	TreeNode(int x) {
+		val = x;
+	}
+}
+ 
+public class Solution {
+	//store max value
+	int max; 
+	public int maxPathSum(TreeNode root) {
+		max = (root == null)? 0: root.val;
+		findMax(root);
+		return max;
+	}
+
+	public int findMax(TreeNode node){
+		if(node == null) return 0;
+
+		//recursively get sum of left and right path
+		int left = Math.max(findMax(node.left), 0);
+		int right = Math.max(findMax(node.right), 0);
+
+		//update maximun here
+		max = Math.max(node.val + left + right, max);
+
+		//return sum of largest path of current node
+		return node.val + Math.max(left, right);
+	}
+}
+
+
+//We can also use an array to store value for recursive methods.
+public class Solution {
+	public int maxPathSum(TreeNode root) {
+		int max[] = new int[1];
+		max[0] = Integer.MIN_VALUE;
+		calculateSum(root, max);
+		return max[0];
+	}
+
+	public int calculateSum(TreeNode root, int[] max){
+		if(root == null) return 0;
+
+		int left = calculateSum(root.left, max);
+		int right = calculateSum(root.right, max);
+
+		int current = Math.max(root.val, Math.max(root.val + left, root.val + right));
+
+		max[0] = Math.max(max[0], Math.max(current, left + root.val + right));
+
+		return current;
+	}
+}
--- Binary Tree Preorder Traversal/BinaryTreePreorderTraversal.java
+++ Binary Tree Preorder Traversal/BinaryTreePreorderTraversal.java
@@ -14,28 +14,47 @@ return [1,2,3].
 Note: Recursive solution is trivial, could you do it iteratively?
 */
 
-public class Solution{
-	public ArrayList<Integer> preorderTraversal(TreeNode root){
-		ArrayList<Integer> ret = new ArrayList<Integer>();
+public class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        ArrayList<Integer> tree = new ArrayList<Integer>();
+        if(root == null) return tree;
+        
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        stack.push(root);
+        
+        while(stack.size() > 0){
+            TreeNode curr = stack.pop();
+            tree.add(curr.val);
+            
+            if(curr.right != null){
+                stack.push(curr.right);
+            }
+            
+            if(curr.left != null){
+                stack.push(curr.left);
+            }
+        }
+        return tree;
+    }
+}
 
-		if(root == null)
-			return ret;
-
-		Stack<TreeNode> stack = new Stack<TreeNode>();
-		stack.push(root);
-
-		while(!stack.empty()){
-			TreeNode cur = stack.pop();
-			ret.add(cur.val);
-
-			if(cur.right!=null){
-				stack.push(cur.right);
-			}
-
-			if(cur.left!=null){
-				stack.push(cur.left);
-			}
-		}
-		return ret;
-	}
+//my recursion solution
+public class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        ArrayList<Integer> tree = new ArrayList<Integer>();
+        if(root == null) return tree;
+        
+        helper(tree, root);
+        return tree;
+    }
+    
+    public void helper(ArrayList<Integer> tree, TreeNode node){
+        if(node == null) return;
+        
+        tree.add(node.val);
+        
+        helper(tree, node.left);
+        
+        helper(tree, node.right);
+    }
 }
