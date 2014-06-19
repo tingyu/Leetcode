caa4857c296fe90c3be0552fde24777d79998934
tingyu <ytcleartime@gmail.com>
2014-06-18 20:38:26 -0700

    Balanced

--- Balanced Binary Tree/BalancedBinaryTree.java
+++ Balanced Binary Tree/BalancedBinaryTree.java
@@ -20,6 +20,27 @@ A typical recursive problem for solving tree problems.
  *     TreeNode(int x) { val = x; }
  * }
  */
+public class Solution {
+    public boolean isBalanced(TreeNode root) {
+        if(root == null) return true;
+        
+        if(root.left == null && root.right == null) return true;
+        
+        if(Math.abs(depth(root.left) - depth(root.right)) > 1){
+            return false;
+        }
+        
+        return isBalanced(root.left)&&isBalanced(root.right);
+    }
+    
+    public int depth(TreeNode root){
+        if(root == null) return 0;
+        
+        return 1 + Math.max(depth(root.left), depth(root.right));
+    }
+}s
+
+
  public class Solution{
  	public boolean isBalanced(TreeNode root) {
  		if(root == null)
