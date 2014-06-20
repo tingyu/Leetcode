b4a387e5ffe1460c1bfc03dbcd0ecf9a50921005
tingyu <ytcleartime@gmail.com>
2014-06-20 15:40:28 -0700

    Construct Binary Tree from Inorder and Preorder Traversal

--- Construct Binary Tree from Inorder and Postorder Traversal/ConstructBinaryTreefromInorderandPostorderTraversal.java
+++ Construct Binary Tree from Inorder and Postorder Traversal/ConstructBinaryTreefromInorderandPostorderTraversal.java
@@ -9,6 +9,7 @@ You may assume that duplicates do not exist in the tree.
 
 /*
 Throughts
+Solution: Divide and Conquer
 
 This problem can be illustrated by using a simple example.
 
@@ -19,6 +20,8 @@ Then we can identify the left and right sub-trees of the root from in-order arra
 
 Using the length of left sub-tree, we can identify left and right sub-trees in post-order array. 
 Recursively, we can build up the tree.
+
+Detailed description: http://leetcode.com/2011/04/construct-binary-tree-from-inorder-and-preorder-postorder-traversal.html
 */
 
 /**
@@ -32,6 +35,38 @@ Recursively, we can build up the tree.
  */
 public class Solution {
     public TreeNode buildTree(int[] inorder, int[] postorder) {
-        
+        int inStart = 0; 
+        int inEnd = inorder.length - 1;
+        int postStart = 0;
+        int postEnd = postorder.length - 1;
+
+        return buildTree(inorder, inStart, inEnd, postorder, postStart, postEnd);
+    }
+
+    public TreeNode buildTree(int[] inorder, int inStart, int inEnd, int[] postorder, int postStart, int postEnd){
+    	if(inStart > inEnd || postStart > postEnd){
+    		return null;
+    	}
+
+    	int rootValue = postorder[postEnd];
+    	TreeNode root = new TreeNode(rootValue);
+
+    	//find the index in inorder that is the root
+    	int k = 0;
+    	for(int i = 0; i < inorder.length; i++){
+    		if(inorder[i] == rootValue){
+    			k = i;
+    			break;
+    		}
+    	}
+
+    	//because k is not the length, it it need to -(inStart + 1) to get the length
+    	//the start and the end are the left sub tree
+    	root.left = buildTree(inorder, inStart, k -1, postorder, postStart, postStart+k-(inStart + 1));
+    	//postStart + k - inStart = postStart + k - (inStart + 1) + 1 this the postStart for right sub tree
+    	root.right = buildTree(inorder, k + 1, inEnd, postorder, postStart + k - inStart, postEnd -1);
+
+    	return root;
+
     }
 }
---
+++ Construct Binary Tree from Preorder and Inorder Traversal/ConstructBinaryTreefromPreorderandInorderTraversal.java
@@ -0,0 +1,59 @@
+/**
+Construct Binary Tree from Preorder and Inorder Traversal
+
+Given preorder and inorder traversal of a tree, construct the binary tree.
+
+Note:
+You may assume that duplicates do not exist in the tree.
+*/
+
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+/*
+Solution: Divide and Conquer
+According to the rule of preorder traversal,  the first item in the preorder array must be the root. 
+and the question also told us "You may assume that duplicates do not exist in the tree." 
+so we can go through inorder array find the root's position, then  we got left tree and right tree. 
+finally we can apply recursion to got the tree we want base on above logic.
+*/
+public class Solution {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        int preStart = 0;
+        int preEnd = preorder.length -1;
+        int inStart = 0;
+        int inEnd = inorder.length -1;
+        
+        return buildTree(preorder, preStart, preEnd, inorder, inStart, inEnd);
+    }
+    
+    public TreeNode buildTree(int[] preorder, int preStart, int preEnd, int[] inorder, int inStart, int inEnd){
+        if(preStart > preEnd || inStart > inEnd){
+            return null;
+        }
+        
+        int rootValue = preorder[preStart];
+        TreeNode root = new TreeNode(rootValue);
+        
+        int k = 0;
+        for(int i = 0; i < inorder.length; i++){
+            if(inorder[i] == rootValue){
+                k = i;
+                break;
+            }
+        }
+        
+        root.left = buildTree(preorder, preStart + 1, preStart + 1 + k - (inStart + 1), inorder, inStart, k -1);
+        
+        root.right = buildTree(preorder, preStart + k -inStart + 1, preEnd, inorder, k + 1, inEnd);
+        
+        return root;
+    }
+}
