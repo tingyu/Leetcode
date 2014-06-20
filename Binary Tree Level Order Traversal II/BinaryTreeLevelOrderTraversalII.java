b6d07c182285e2307a52add9059eef67a993a0cf
tingyu <ytcleartime@gmail.com>
2014-06-19 21:14:46 -0700

    add queue solution

--- Binary Tree Level Order Traversal II/BinaryTreeLevelOrderTraversalII.java
+++ Binary Tree Level Order Traversal II/BinaryTreeLevelOrderTraversalII.java
@@ -121,3 +121,33 @@ public class Solution{
 		helper(node.right, depth + 1, tree);
 	}
 }
+
+
+
+//queue
+public class Solution {
+    public ArrayList<ArrayList<Integer>> levelOrderBottom(TreeNode root) {
+        ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
+        ArrayList<Integer> list = null;
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        if(root != null)
+            queue.offer(root);
+        int count = 0;
+        while(!queue.isEmpty()){
+            if(count == 0){
+                list = new ArrayList<Integer>();
+                res.add(list);
+                count = queue.size();
+            }            
+            TreeNode node = queue.poll();
+            list.add(node.val);
+            count--;
+            if(node.left != null)
+                queue.offer(node.left);
+            if(node.right != null)
+                queue.offer(node.right);
+        }
+        Collections.reverse(res);
+        return res;
+    }
+}
