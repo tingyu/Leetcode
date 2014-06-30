6a2ba8616b06a64a728c9f1481fc786bb971ac2e
tingyu <ytcleartime@gmail.com>
2014-06-30 10:22:21 -0700

    modify

--- Letter Combinations of a Phone Number/LetterCombinationsofaPhoneNumber.java
+++ Letter Combinations of a Phone Number/LetterCombinationsofaPhoneNumber.java
@@ -14,28 +14,29 @@ Note:
 Although the above answer is in lexicographical order, your answer could be in any order you want.
 */
 
+
 public class Solution {
     public List<String> letterCombinations(String digits) {
-		String[] ss = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-
-		ArrayList<String> ret = new ArrayList<String>();
-		rec(ret, digits.length(), ss, digits, new StringBuffer());
-		return ret;        
+        String[] ss = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        
+        ArrayList<String> res = new ArrayList<String>();
+        helper(res, digits.length(), ss, digits, new StringBuffer());
+        return res;
     }
-
-    public void rec(ArrayList<String> ret, int remain, String[] ss, String digits, StringBuffer sb){
-    	//说明对digits遍历结束
-    	if(remain == 0){
-    		ret.add(sb.toString());
-    		return;
-    	}
-
-    	String s = ss[digits.charAt(0) - '0'];//得到digits[0]对应的String s
-    	//DFS
-    	for(int i = 0; i < s.length(); i++){
-    		sb = sb.append(s.charAt(i));
-    		rec(ret, remain - 1, ss, digits.substring(1), sb);
-    		sb.deleteCharAt(sb.length() -1); //恢复现场
-    	}
+    
+    public void helper(ArrayList<String> res, int remain, String[] ss, String digits, StringBuffer tmp){
+        //说明对digits遍历结束
+        if(remain == 0){
+            res.add(tmp.toString());
+            return;
+        }
+        
+        String s = ss[digits.charAt(0) - '0'];//得到digits[0]对应的String s
+        //DFS
+        for(int i = 0; i < s.length(); i++){
+            tmp.append(s.charAt(i));
+            helper(res, remain - 1, ss, digits.substring(1), tmp);
+            tmp.deleteCharAt(tmp.length() - 1);//恢复现场
+        }
     }
 }
