b53f4b1dcafca3bab34e4dd94f1dc5b250348f1c
tingyu <ytcleartime@gmail.com>
2014-07-02 12:03:14 -0700

    Scramble String

---
+++ First Missing Positive/FirstMissingPositive.java
@@ -0,0 +1,77 @@
+/**
+First Missing Positive 
+
+Given an unsorted integer array, find the first missing positive integer.
+
+For example,
+Given [1,2,0] return 3,
+and [3,4,-1,1] return 2.
+
+Your algorithm should run in O(n) time and uses constant space.
+*/
+
+//my solution:
+//注意可能有重复的数字，去掉各种可能的情况
+public class Solution {
+    public int firstMissingPositive(int[] A) {
+        if(A == null || A.length ==0)
+            return 1;
+            
+        Arrays.sort(A);
+        
+        //如果最小的都大于1，直接返回1就可以
+        if(A[0] > 1){
+            return 1;
+        }
+
+        //从1到结尾
+        for(int i = 1; i < A.length; i++){
+        	//如果两个相等或者两个相差为1，或者当前的都小于0，或者当前的是1，前一个是-1（此时差的是0）；这些情况下continue
+            if(A[i] - A[i-1] == 1 || A[i] == A[i-1] || A[i] < 0 || (A[i] == 1 && A[i-1] == -1)){
+                continue;
+            }else if(A[i-1] <= -1 && A[i] > 0 && A[i] >1){//如果前一个比-1小，后一个大于1，返回1
+                return 1;   
+            }else if(A[i-1]>=0){//其他情况，当所有比较的都>=0的时候，如果相差大于1，那么返回小的+1
+                return A[i-1] +1;
+            }
+        }
+        //以上情况都不成立，则说明一直都是连续的直到最后一个
+        return A[A.length -1] + 1;
+    }
+}
+/*
+other solutions:
+http://blog.csdn.net/kenden23/article/details/17099987
+ 这条题目虽然简单，但是思路还是很多的，可以开拓一下思路。
+下面三种思路都是O(n)时间复杂度，测试运行时间基本上都没区别：
+1 排序之后查找
+2 把出现的数值放到与下标一致的位置，再判断什么位置最先出现不连续的数值，就是答案了。
+3 和2差不多，把出现过的数值的下标位置做好标识，如果没有出现过的数组的下标就没有标识，那么这个就是答案。
+ 第一个思路最简单了
+
+ Another solution:
+ http://blog.csdn.net/linhuanmars/article/details/20884585
+ 这道题要求用线性时间和常量空间，思想借鉴到了Counting sort中的方法，不了解的朋友可以参见Counting sort - Wikipedia。
+ 既然不能用额外空间，那就只有利用数组本身，跟Counting sort一样，利用数组的index来作为数字本身的索引，
+ 把正数按照递增顺序依次放到数组中。即让A[0]=1, A[1]=2, A[2]=3, ... , 这样一来，
+ 最后如果哪个数组元素违反了A[i]=i+1即说明i+1就是我们要求的第一个缺失的正数。对于那些不在范围内的数字，
+ 我们可以直接跳过，比如说负数，0，或者超过数组长度的正数，这些都不会是我们的答案。
+ */
+
+
+ public int firstMissingPositive(int[] A) { 
+ 	if(A == null || A.length == 0){
+ 		return 1;
+ 	}
+
+ 	for(int i = 0; i < A.length; i++){
+ 		if(A[i] <= A.length && A[i] > 0 && A[A[i] - 1] != A[i]){
+ 			int temp = A[A[i] - 1];
+ 			A[A[i] - 1] = A[i];
+ 			A[i] = temp;
+ 			i--;
+ 		}
+ 	}
+
+ 	
+ }
---
+++ Scramble String/ScrambleString.java
@@ -0,0 +1,128 @@
+/**
+Scramble String
+
+Given a string s1, we may represent it as a binary tree by partitioning it to two non-empty substrings recursively.
+
+Below is one possible representation of s1 = "great":
+
+    great
+   /    \
+  gr    eat
+ / \    /  \
+g   r  e   at
+           / \
+          a   t
+To scramble the string, we may choose any non-leaf node and swap its two children.
+
+For example, if we choose the node "gr" and swap its two children, it produces a scrambled string "rgeat".
+
+    rgeat
+   /    \
+  rg    eat
+ / \    /  \
+r   g  e   at
+           / \
+          a   t
+We say that "rgeat" is a scrambled string of "great".
+
+Similarly, if we continue to swap the children of nodes "eat" and "at", it produces a scrambled string "rgtae".
+
+    rgtae
+   /    \
+  rg    tae
+ / \    /  \
+r   g  ta  e
+       / \
+      t   a
+We say that "rgtae" is a scrambled string of "great".
+
+Given two strings s1 and s2 of the same length, determine if s2 is a scrambled string of s1.
+*/
+
+/*
+http://www.blogjava.net/sandy/archive/2013/05/22/399605.html
+分析：
+
+这个问题是google的面试题。由于一个字符串有很多种二叉表示法，貌似很难判断两个字符串是否可以做这样的变换。
+对付复杂问题的方法是从简单的特例来思考，从而找出规律。
+先考察简单情况：
+字符串长度为1：很明显，两个字符串必须完全相同才可以。
+字符串长度为2：当s1="ab", s2只有"ab"或者"ba"才可以。
+对于任意长度的字符串，我们可以把字符串s1分为a1,b1两个部分，s2分为a2,b2两个部分，满足（(a1~a2) && (b1~b2)）或者 （(a1~b2) && (a1~b2)）
+
+如此，我们找到了解决问题的思路。首先我们尝试用递归来写。
+
+
+解法一（递归）
+
+两个字符串的相似的必备条件是含有相同的字符集。简单的做法是把两个字符串的字符排序后，然后比较是否相同。
+加上这个检查就可以大大的减少递归次数。
+//为什么下面的方法考虑了就全了呢？？？？
+Scramble的定义就是要不一样，要不swap了。所以两种递归
+*/
+
+public class Solution {
+    public boolean isScramble(String s1, String s2) {
+        int l1 = s1.length();
+        int l2 = s2.length();
+
+        //前面先处理一系列的特殊情况来减少运算
+
+        //如果两个长度不等，肯定false，不用往后看了
+        if(l1 != l2){
+          return false;
+        }        
+
+        //这里隐含了l1 = l2，如果l1 == 0那么返回true
+        if(l1 == 0){
+          return true;
+        }
+
+        //把string 变成charArray来看下排序完之后的两个序列是不是相等，如果不等，不用考虑其他的了，直接返回false
+        char[] c1 = s1.toCharArray();
+        char[] c2 = s2.toCharArray();
+
+        if(l1 == 1){
+          return c1[0] == c2[0];
+        }
+
+        Arrays.sort(c1);
+        Arrays.sort(c2);
+
+        for(int i = 0; i < l1; i++){
+          if(c1[i] != c2[i]){
+            return false;
+          }
+        }
+
+        //在去掉上面那些特殊情况之后，再进一步判断s2是不是由s1转换而来的。注意这里用的是s1, s2不是c1, c2，也就是没有排序的
+        boolean result = false;
+        for(int i = 1; i < l1 && !result; i++){
+          String s11 = s1.substrings(0, i);
+          String s12 = s1.substrings(i);
+          String s21 = s2.substrings(0, i);
+          String s22 = s2.substrings(i);
+          result = isScramble(s11, s21) && isScramble(s12, s22);
+          //前面的若s1=great, s2=regeat，那么s11=g,s12=reat; s21=r, s22=egeat
+          //前面的如果不成立，然后考虑swap之后的情况，这样可以减少循环。这里使用s11和s32比较，s12和s31比较。
+          //s31=rgea, s32=t，防止是great在g后面切一刀变成reatg的情况
+          if(!result){
+            String s31 = s2.substrings(0, l1 - i);
+            String s32 = s2.substrings(l1 - i);
+            result = isScramble(s11, s32) && isScramble(s12, s31);
+          }
+        }
+        return result;
+    }
+}
+
+/*
+解法二（动态规划）
+减少重复计算的方法就是动态规划。动态规划是一种神奇的算法技术，不亲自去写，是很难完全掌握动态规划的。
+
+这里我使用了一个三维数组boolean result[len][len][len],其中第一维为子串的长度，第二维为s1的起始索引，第三维为s2的起始索引。
+result[k][i][j]表示s1[i...i+k]是否可以由s2[j...j+k]变化得来。
+*/
+
+
+
