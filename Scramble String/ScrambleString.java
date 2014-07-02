ccd4a4785331818f6f6723cc5ec73d56e087d4ba
tingyu <ytcleartime@gmail.com>
2014-07-02 12:28:35 -0700

    add solutions

--- Scramble String/ScrambleString.java
+++ Scramble String/ScrambleString.java
@@ -124,5 +124,41 @@ public class Solution {
 result[k][i][j]表示s1[i...i+k]是否可以由s2[j...j+k]变化得来。
 */
 
+public class Solution {
+    public boolean isScramble(String s1, String s2) {
+        int len = s1.length();
+        if(len != s2.length()){
+          return false;
+        }
+
+        if(len == 0){
+          return true;
+        }
+
+        char[] c1 = s1.toCharArray();
+        char[] c2 = s2.toCharArray();
+
+        boolean[][][] result = new boolean[len][len][len];
+        //初始化，因为子串的长度是0，表示s1[i]是否可以由s2[j]变化得来。一个元素是否能由另外一个元素变化而来，就靠看他们是不是相等的了。
+        for(int i = 0; i < len; i++){
+          for(int j = 0; j < len; j++){
+              result[0][i][j] = (c1[i] == c2[j]);
+          }
+        }
+
+        for(int k = 2; k <= len; k++){
+          for(int i = len - k; i >=0; i--){
+            for(int j = len - k; j >= 0; j--){
+                boolean r = false;
+                for(int m = 1; m < k && !r; m++){
+                  r = (result[m - 1][i][j] && result[k-m-1][i+m][j+m])||(result[m-1][i][j+k-m]&&result[k-m-1][i+m][j]);
+                }
+                result[k-1][i][j] = r;
+            }
+          }
+        }
+        return result[len -1][0][0];
+    }
+}
 
 
