da7aeab6c37a69018c6b4af790eae8bf23a1084c
tingyu <ytcleartime@gmail.com>
2014-07-06 23:40:09 -0700

    Max Points on a Line

---
+++ Max Points on a Line/MaxPointsonaLine.java
@@ -0,0 +1,80 @@
+/**
+Max Points on a Line
+
+Given n points on a 2D plane, find the maximum number of points that lie on the same straight line.
+*/
+
+/*
+http://www.aichengxu.com/article/Java/18179_2.html
+固定一点，计算每一条经过这个店的直线斜率，用一个hashmap记录。记录下最多的斜率个数。对每个点做一次这样的搜索，这样复杂度就是O(n^2)
+*/
+
+/**
+ * Definition for a point.
+ * class Point {
+ *     int x;
+ *     int y;
+ *     Point() { x = 0; y = 0; }
+ *     Point(int a, int b) { x = a; y = b; }
+ * }
+ */
+public class Solution {
+    public int maxPoints(Point[] points) {
+		if(points.length == 0) return 0;
+		if(points.length == 1) return 1;
+		int max = 0;
+		for(int i = 0; i < points.length; i++){
+			HashMap<Float, Integer> hm = new HashMap<Float, Integer>();
+			int same = 0;
+			for(int j = 0; j < points.length; j++){
+				if(i == j) continue;
+				if(points[i].x == points[j].x && points[i].y == points[j].y){
+					same++;
+					continue;
+				}
+
+				float slope = (float)(points[i].y - points[j].y)/(points[i].x - points[j].x);
+				if(hm.containsKey(slope)){
+					hm.put(slope, hm.get(slope) + 1);
+				}else{
+					hm.put(slope, 2);
+				}
+			}
+
+			for(float k: hm.keySet()){
+				hm.put(k, hm.get(k) + same);
+			}
+
+			for(int v: hm.values()){
+				if(v > max) max =v;
+			}
+
+			//process all points are the same situation
+			if(same + 1 > max){
+				max = same + 1;
+			}
+		} 
+		return max;      
+    }
+}
+
+/*
+if there is no same + 1 >max
+
+Submission Result: Wrong Answer
+Input: 	[(0,0),(0,0)]
+Output: 	0
+Expected: 	2
+*/
+
+/*
+another similar solution:
+http://rleetcode.blogspot.com/2014/01/max-points-on-linejava.html
+
+
+others:
+http://yucoding.blogspot.com/2013/12/leetcode-question-max-points-on-line.html
+http://blog.csdn.net/muscler/article/details/23015277
+*/
+
+
---
+++ Wildcard Matching/WildcardMatching.java
@@ -0,0 +1,59 @@
+/**
+Wildcard Matching
+
+Implement wildcard pattern matching with support for '?' and '*'.
+
+'?' Matches any single character.
+'*' Matches any sequence of characters (including the empty sequence).
+
+The matching should cover the entire input string (not partial).
+
+The function prototype should be:
+bool isMatch(const char *s, const char *p)
+
+Some examples:
+isMatch("aa","a") → false
+isMatch("aa","aa") → true
+isMatch("aaa","aa") → false
+isMatch("aa", "*") → true
+isMatch("aa", "a*") → true
+isMatch("ab", "?*") → true
+isMatch("aab", "c*a*b") → false
+*/
+
+/*
+http://blog.csdn.net/perfect8886/article/details/22689147
+
+
+通过率很低的一题，Java没指针写起来是不顺畅。
+
+贪心的策略，能匹配就一直往后遍历，匹配不上了就看看前面有没有'*'来救救场，再从'*'后面接着试。
+*/
+
+public class Solution {
+    public boolean isMatch(String s, String p) {
+		int i = 0; 
+		int j = 0;
+		int star = -1;
+		int mark = -1;
+		while(i < s.length()){
+			if(j < p.length() && (p.charAt(j) == '?' || p.charAt(j) == s.charAt(i))){
+				++i;
+				++j;
+			}else if(j < p.length() && p.charAt(j) == '*'){
+				star = j++;
+				mark = i;
+			}else if(star != -1){
+				j = star + 1;
+				i = ++ mark;
+			}else{
+				return false;
+			}
+		}        
+
+		while(j < p.length() && p.charAt(j) == '*'){
+			++j;
+		}
+		return j == p.length();
+    }
+}
