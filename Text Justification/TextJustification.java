0a4d041115b63b37384cc63b6317000330360636
tingyu <ytcleartime@gmail.com>
2014-07-07 23:31:30 -0700

    add solutions

--- Text Justification/TextJustification.java
+++ Text Justification/TextJustification.java
@@ -83,3 +83,61 @@ public class Solution {
 时间上我们需要扫描单词一遍，然后在找到行尾的时候在扫描一遍当前行的单词，不过总体每个单词不会被访问超过两遍，所以总体时间复杂度是O(n)。
 而空间复杂度则是结果的大小（跟单词数量和长度有关，不能准确定义，如果知道最后行数r，则是O(r*L)）。代码如下： 
 */
+public ArrayList<String> fullJustify(String[] words, int L) {
+    ArrayList<String> res = new ArrayList<String>();
+    if(words==null || words.length==0)
+        return res;
+    int count = 0;
+    int last = 0;
+    for(int i=0;i<words.length;i++)
+    {
+        if(count+words[i].length()+(i-last)>L)
+        {
+            int spaceNum = 0;
+            int extraNum = 0;
+            if(i-last-1>0)
+            {
+                spaceNum = (L-count)/(i-last-1);
+                extraNum = (L-count)%(i-last-1);
+            }
+            StringBuilder str = new StringBuilder();
+            for(int j=last;j<i;j++)
+            {
+                str.append(words[j]);
+                if(j<i-1)
+                {
+                    for(int k=0;k<spaceNum;k++)
+                    {
+                        str.append(" ");
+                    }
+                    if(extraNum>0)
+                    {
+                        str.append(" ");
+                    }
+                    extraNum--;
+                }
+            }
+            for(int j=str.length();j<L;j++)
+            {
+                str.append(" ");
+            }       
+            res.add(str.toString());
+            count=0;
+            last=i;
+        }
+        count += words[i].length();
+    }
+    StringBuilder str = new StringBuilder();
+    for(int i=last;i<words.length;i++)
+    {
+        str.append(words[i]);
+        if(str.length()<L)
+            str.append(" ");
+    }
+    for(int i=str.length();i<L;i++)
+    {
+        str.append(" ");
+    }
+    res.add(str.toString());
+    return res;
+}
