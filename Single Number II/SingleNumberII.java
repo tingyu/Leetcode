274764141ec609f5bf20e0c7bb6541f82c4dbce6
tingyu <ytcleartime@gmail.com>
2014-07-10 16:48:54 -0700

    add solutions

--- Single Number II/SingleNumberII.java
+++ Single Number II/SingleNumberII.java
@@ -28,7 +28,64 @@ public class Solution{
 	}
 }
 
+/*
+http://rleetcode.blogspot.com/2014/01/single-number-ii.html
+Cause of the constrain of linear time complexity, so one loop may be work
+also beacause of the the hint of without using extra memory, so a 32 length int array should be a
+good choice. 
+ 
+use this array to count the appear times of 1 at each bit, use %3 to ignore the number appear 3 times
+then use | to build left number, because only one number appear onece, so the left number is the number is which
+we want.
+*/
+public class Solution {
+ 
+    public int singleNumber(int[] A) {
+        if (A==null||A.length==0){
+            return -1;
+        }
+        
+        int[] bits=new int[32];
+        int result=0;
+        for (int i=0; i<32; i++){
+            for(int j=0; j<A.length; j++){
+                bits[i]+=A[j]>>i&1;
+                bits[i]%=3;
+            }
+            
+            result|=(bits[i]<<i);
+        }
+        
+        return result;
+            
+   }
+        
+}
 
+/*
+http://gongxuns.blogspot.com/2013/10/leetcode-single-number-ii.html
+Idea: if we look all the last bit of the numbers (assuming all are 32-bit) in the array, 
+there must be 3k+1 or 3k `1's in total depending whether the single number's last bit is one or zero. 
+This observation holds for all the rest 31 bits as well. Hence, if we sum all the numbers only 
+at certain bit and mod by 3, we can get the corresponding bit the single number. 
+Do this for all 32-bit, we can get all bits of that number. This generalizes the solution of LeetCode: 
+Single Number I, where xor all the numbers is essentially trying to add all bits and then mod by 2...
+*/
+class Solution {
+public:
+    int singleNumber(int A[], int n) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        int single_number = 0;
+        for (int i = 0; i < 32; ++i) {// assume dealing with int32.
+            int bit = 0;
+            for (int j = 0; j < n; ++j) {
+                bit = (bit + ((A[j] >> i) & 1)) % 3;
+            }
+            single_number += (bit << i);
+        }
+        return single_number;
+    }
+};
 //O(n) 时间复杂度，O(1)空间复杂度
 
 public class Solution{
