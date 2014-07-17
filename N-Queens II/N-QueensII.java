ea2ab4677ed5307ab6aefc21f8e70e86dca8fad8
tingyu <ytcleartime@gmail.com>
2014-07-17 14:18:21 -0700

    add solutions

--- N-Queens II/N-QueensII.java
+++ N-Queens II/N-QueensII.java
@@ -6,6 +6,41 @@ Follow up for N-Queens problem.
 Now, instead outputting board configurations, return the total number of distinct solutions.
 */
 
+//my solution
+public class Solution {
+    int res;
+    public int totalNQueens(int n) {
+        //int res = 0;
+        res = 0;
+        int[] position = new int[n];
+        helper(position, 0, n);
+        return res;
+    }
+    
+    private void helper(int[] position, int row, int n){
+        if(row == n){
+            res++;
+            return;
+        }
+        
+        for(int j = 0; j < n; j++){
+            position[row] = j;
+            if(check(position, row)){
+                helper(position, row + 1, n);
+            }
+        }
+    }
+    
+    private boolean check(int[] position, int row){
+        for(int i = 0; i < row; i++){
+            if(position[i] == position[row] || Math.abs(position[row] - position[i]) == (row -i)){
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
 //同样的思路，代码反而比Queen I 要简单，记得把res声明在totalNQueens外面
 
 public class Solution {
