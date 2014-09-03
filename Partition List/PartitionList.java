2848878870eb9fa3a4b5d3c5de5e7d671a3a7976
tingyu <ytcleartime@gmail.com>
2014-09-02 19:01:32 -0700

    add solution

--- Partition List/PartitionList.java
+++ Partition List/PartitionList.java
@@ -10,6 +10,22 @@ Given 1->4->3->2->5->2 and x = 3,
 return 1->2->2->4->3->5. 
 */
 /*
+summary:
+这题可以采用三种解法来解。基本的思路都是新建两个linkedlist,然后遍历原来的LinkedList,并且不断把值插入到这两个新的linkedList中，最后
+merge这两个linkedList
+三个方法的差别之处：
+1. 新建了两个fakeHead1,fakeHead2.然后后让p1，p2分别遍历这两个fakeHead，同时让p遍历原来的linkedlist。
+需要注意的是 分离出next，然后把当前结点孤立起来。此时然后再p1, p2指向就不会出错了
+这种解法写法最简单
+    另外还有个根据node的值不断新建结点的做法。感觉是复制了值，这种解法在面试时可能不大好，有些作弊嫌疑
+2. CC150上面的弄了四个值beforeStart，beforeEnd, afterStart, afterEnd。然后对两个新建的链表又需要采用两种情况讨论。
+如果beforeStart是空，不是空分开讨论。
+写法比上一个复杂。思路倒是还比较明确
+3. CC150上面的弄了2个值beforeStart，afterStart。然后每次都插入到beforeStart, afterStart前面，并且不断更新beforeStart, afterStart
+这种做法在CC150上面是可以使用的，但是如果在leetcode上面对于这题而言就错了。因为不断插入到前面就会导致逆序了。不满足题目要求
+可以借鉴用来开拓思路
+
+
 自己开始写的解法错了。看来准确性急需要提高。
 自己解法里面指针指来指去就错了。只能用利用值创建一个新链表的方法，空间是O(n),时间也是O(n)
                 //p1.next = p;
@@ -18,8 +34,36 @@ CC150书上面有两种解法
 1. We iterate through the linked list, inserting elements into our before list or our after list. 
 Once we reach the end of the linked list and have completed this splitting, we merge 
 the two lists.
+
 */
 //my solution
+//根据CC150上面的启示，分离出next，然后把当前结点孤立起来。此时然后再p1, p2指向就不会出错了
+public class Solution {
+    public ListNode partition(ListNode head, int x) {
+        ListNode head1 = new ListNode(0);
+        ListNode head2 = new ListNode(x);
+        ListNode p1 = head1;
+        ListNode p2 = head2;
+        ListNode p = head;
+        
+        while(p != null){
+            ListNode next = p.next;
+            p.next = null;
+            if(p.val < x){
+                p1.next = p;
+                p1 = p1.next;
+            }else{
+                p2.next = p;
+                p2 = p2.next;
+            }
+            p = next;
+        }
+        
+        p1.next = head2.next;
+        return head1.next;
+    }
+}
+
 //开始想的是插入和删除的操作，弄两个新的链表。但是这样很容易出错。所以，如果是弄两个fakeHead，然后不断new新的结点在后面，然后把两个链表合并，
 //这样会容易的多
 public class Solution {
@@ -107,6 +151,7 @@ and one for elements greater than or equal to x.
 We iterate through the linked list, inserting elements into our before list or our after list. 
 Once we reach the end of the linked list and have completed this splitting, we merge the two lists.
 The code below implements this approach
+关键是要先记录下来next结点，然后把当前的node孤立起来
 */
 
 //pass in the head of the linked list and value to partition around
